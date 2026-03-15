3dc3bdb50ffb7531050dc8642e6e648ea8ea909c
Gayatri-beep <104546498@student.swin.edu.au>
2026-03-16 10:29:54 +1100

    Added Player code - Manage Success + Health Bars, and added Method called by Attack object
    Added Player code :
    - Initialised health + success bars
    - Added method called by Attack object (does damage or builds success depending on win/fail)
    - Added debug code

--- Phantom vs Opera/Assets/Scripts/Player/Player.cs
+++ Phantom vs Opera/Assets/Scripts/Player/Player.cs
@@ -1,16 +1,114 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // # DELETE ALL - "DELETE"
+    // # Also refine comments - DELETE
+
+        private bool _isAlive; //idk what type - DELETE
+        private bool _hasWon; //idk what type - DELETE
+        private int _healthBar;
+        private int _successBar;
+        private string _sceneGameOver = "Game Over";
+        private string _sceneMainMenu = "Main Menu";
+
+    //Set up Initial health/success levels
     void Start()
     {
-        
+        _healthBar = 10;
+        _successBar = 0;
+        _isAlive = true;
+        _hasWon = false;
+
+        Debug.Log("Starting health: " + _healthBar);
+        Debug.Log("Starting success: " + _successBar);
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsAlive //dk what type - DELETE
     {
-        
+        get { return _isAlive; }
+    }
+
+    public bool HasWon //dk what type - DELETE
+    {
+        get { return _hasWon; }
+    }
+
+    //Method to Load GameOver Scene 
+    public void LoadSceneGameOver()
+    {
+        if (string.IsNullOrEmpty(_sceneGameOver))
+        {
+            Debug.LogWarning("Scene name is null or empty");
+        }
+        else
+        {
+            SceneManager.LoadScene(_sceneGameOver);
+        }
+    }
+
+    //Method to Load MainMenu Scene 
+    public void LoadSceneMainMenu()
+    {
+        if (string.IsNullOrEmpty(_sceneMainMenu))
+        {
+            Debug.LogWarning("Scene name is null or empty");
+        }
+        else
+        {
+            SceneManager.LoadScene(_sceneMainMenu);
+        }
+    }
+
+    //Method to Manage Health Bar - sets initial health bar level, sets results for losing all health (i.e. losing game)
+    public void ManageHealthBar()
+    {
+        if (_healthBar <= 0)
+        {
+            _isAlive = false;
+            Debug.Log("isAlive status: " + _isAlive);
+            Debug.Log("initial health bar: " + _healthBar);
+            LoadSceneGameOver();
+        }
+    }
+
+    //Method to Manage Success Bar - sets initial success bar level, sets results for reaching certain success level (i.e. winning game)
+    public void ManageSuccessBar()
+    {
+        if (_successBar >= 10)
+        {
+            _hasWon = true;
+            Debug.Log("hasWon status: " + _hasWon);
+            Debug.Log("initial success bar: " + _successBar);
+            LoadSceneMainMenu();
+        }
+    }
+
+    //Method called by Attack object - on fail = takes damage, on success = builds success 
+    public void HandleAttackResult(string outcome)
+    {
+        if (outcome == "success")
+        {
+            _successBar ++;
+            Debug.Log("success bar: " + _successBar);
+        }
+
+        else
+        {
+            _successBar --;
+            _healthBar --;
+            Debug.Log("success bar: " + _successBar);
+            Debug.Log("health bar: " + _healthBar);
+        }
+
+        ManageHealthBar();
+        ManageSuccessBar();
     }
 }
+
+///Player should have isAlive & hasWon properties GameManager can check based on number of success/losses
+///Player should include a method called by the Attack object to TAKE damage on a fail, or BUILD success master on success
+///When this method is called, Player should check if it meets conditions to change isAlive or hasWon
+///Will take in (string success/fail) as a perm and react accordingly
+///Console or onscreen result should be included when method is called so user can see outcome (or a bar if you're feeling fancy)
