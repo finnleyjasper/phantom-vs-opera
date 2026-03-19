d0d232d216881dfab71595715f5f48feb131287c
Gayatri-beep <104546498@student.swin.edu.au>
2026-03-19 21:50:57 +1100

    Created new script for Player bars UI
    Created new script for Player bars UI

---
+++ Phantom vs Opera/Assets/Scripts/Player/PlayerBarUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBarUI : MonoBehaviour
+{
+    //DELETE !! 
+    //Private Variables 
+        private int _maxHealth = 10;
+        private int _maxSuccess = 10; //dk if this is 10 - DELETE
+
+    public Player player;
+
+
+
+
+
+    void Start() //dk if needs this - DELETE
+    {
+        
+    }
+
+    void Update() //dk if needs this - DELETE
+    {
+        
+    }
+}
