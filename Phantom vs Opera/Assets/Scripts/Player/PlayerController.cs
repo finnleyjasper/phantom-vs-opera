4a5244fbb5820db32a3325364fbbcd631577925d
Gayatri-beep <104546498@student.swin.edu.au>
2026-03-24 21:26:40 +1100

    Implemented Player Movement (Right/Left)
    Implemented Player Movement :
    - Moves right + left via WASD or arrow keys
    - Added speed and used Vector3

--- Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
+++ Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
@@ -8,8 +8,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _xMovement;
     [SerializeField] private float _zMovement;
 
-    [Header("Up & Down Movement")]
-    [SerializeField] private float _yMovement;
 
     //Player's speed 
     [Header("Speed")]
@@ -20,10 +18,9 @@ public class PlayerController : MonoBehaviour
         //Receive position input from Player - is this correct ??? - Delete
         float _xMovement = Input.GetAxis("Horizontal");
         float _zMovement = Input.GetAxis("Vertical");
-        float _yMovement = Input.GetAxis("Vertical"); //DONT KNWO WHAT TO NAME - delete
 
-        Vector3 playerMove = new Vector3(_xMovement, _yMovement, _zMovement);
+        Vector3 playerMove = new Vector3(_xMovement, 0.0f, _zMovement);
 
-        transform.position += playerMove * _playerSpeed;
+        transform.Translate(playerMove * _playerSpeed);
     }
 }
