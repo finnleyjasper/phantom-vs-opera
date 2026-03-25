7ade277609dd395fbface1f240c7e5c705f1a28c
Gayatri-beep <104546498@student.swin.edu.au>
2026-03-25 17:02:38 +1100

    Rough draft of Player Movement
    Rough draft of Player Movement :
    - Added editable keycode values

--- Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
+++ Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
@@ -1,34 +1,141 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables for Player Right + Left Movement 
+        private float _xMovement = 0;
+        private float _zMovement = 0;
 
-    // Private variables for Keyboard Inputs 
+    // Variables for Editable KeyCodes 
     [Header("Left & Right Movement")]
-    [SerializeField] private float _xMovement;
-    [SerializeField] private float _zMovement;
+    [SerializeField] private KeyCode _xLeftKeyCode;
+    [SerializeField] private KeyCode _xRightKeyCode;
+    [SerializeField] private KeyCode _zFrontKeyCode;
+    [SerializeField] private KeyCode _zBackKeyCode;
+
+    [Header("Jump Keycode")]
+    [SerializeField] private KeyCode _jumpKeyCode = KeyCode.Space; 
 
 
-    //Player's speed 
+    // Player's speed + force
     [Header("Speed")]
-    [SerializeField] float _playerSpeed = 0.1f;
+    [SerializeField] private float _playerSpeed = 0.1f;
 
-    //Varibale to get Player's RigidBody Component
-    private Rigidbody _player_RigidBody;
+    [Header("Speed")]
+    [SerializeField] private float _playerForce = 500f;
+
+    // Varibale to get Player's RigidBody Component
+        private Rigidbody _player_RigidBody;
+
+    public void PlayerMove()
+    {
+        if (Input.GetKeyDown(_xLeftKeyCode))
+        {
+            _xMovement--;
+        }
+
+        if (Input.GetKeyDown(_xRightKeyCode))
+        {
+            _xMovement++;
+        }
+
+        if (Input.GetKeyDown(_zFrontKeyCode))
+        {
+            _zMovement--;
+        }
+
+        if (Input.GetKeyDown(_zBackKeyCode))
+        {
+            _zMovement++;
+        }
+
+        Vector3 playerMove = new Vector3(_xMovement, 0.0f, _zMovement);
+
+        transform.Translate(playerMove * _playerSpeed);
+    }
+
+    public void PlayerJump()
+    {
+        if (Input.GetKeyDown(_jumpKeyCode))
+        {
+            _player_RigidBody.AddForce(Vector3.up  * _playerForce);
+        }
+    }
 
     void Start()
     {
-        _player_RigidBody = GetComponent<Rigidbody>();
+        _player_RigidBody = GetComponent<Rigidbody>(); // Get Player's RigidBody Component
     }
 
-    void Update() // need this or? - Delete
+    void Update()
     {
-        //Receive position input from Player - is this correct ??? - Delete
-        float _xMovement = Input.GetAxis("Horizontal"); //Assigns ad key or left/right arrow key
+        PlayerMove();
+        PlayerJump();
+    }
+}
+
+/*
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables for Player Right + Left Movement 
+        private float _xMovement;
+        private float _zMovement;
+
+    // Variables for Editable KeyCodes 
+    [Header("Left & Right Movement")]
+    [SerializeField] private List<KeyCode> _xKeyCodes;
+    [SerializeField] private List<KeyCode> _zKeyCodes;
+
+    [Header("Jump Keycode")]
+    [SerializeField] private KeyCode _jumpKeyCode = KeyCode.Space; 
+
+
+    // Player's speed + force
+    [Header("Speed")]
+    [SerializeField] private float _playerSpeed = 0.1f;
+
+    [Header("Speed")]
+    [SerializeField] private float _playerForce = 500f;
+
+    // Varibale to get Player's RigidBody Component
+        private Rigidbody _player_RigidBody;
+
+    public void PlayerMove()
+    {
+
+        // Receive position input from Player - is this correct ??? - Delete
+        float _xMovement = Input.GetAxis(_xKeyCodes); //Assigns ad key or left/right arrow key
         float _zMovement = Input.GetAxis("Vertical"); //Assigns ws or up/down arrow key 
 
         Vector3 playerMove = new Vector3(_xMovement, 0.0f, _zMovement);
 
         transform.Translate(playerMove * _playerSpeed);
     }
+
+    public void PlayerJump()
+    {
+        if (Input.GetKeyDown(_jumpKeyCode))
+        {
+            _player_RigidBody.AddForce(Vector3.up  * _playerForce);
+        }
+    }
+
+    void Start()
+    {
+        _player_RigidBody = GetComponent<Rigidbody>(); // Get Player's RigidBody Component
+    }
+
+    void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+    }
 }
+
+
+*/
