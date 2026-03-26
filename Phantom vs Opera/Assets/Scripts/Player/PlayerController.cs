9d5483fdc5bf186eaae76097e58fd0406dccbcbd
Gayatri-beep <104546498@student.swin.edu.au>
2026-03-27 01:02:12 +1100

    Fixed Player Movement
    Fixed Player Movement

--- Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
+++ Phantom vs Opera/Assets/Scripts/Player/PlayerController.cs
@@ -30,87 +30,28 @@ public class PlayerController : MonoBehaviour
 
     public void PlayerMove()
     {
-        if (Input.GetKeyDown(_xLeftKeyCode))
+        _xMovement = 0;
+        _zMovement = 0;
+
+        if (Input.GetKey(_xLeftKeyCode))
         {
             _xMovement--;
         }
 
-        if (Input.GetKeyDown(_xRightKeyCode))
+        if (Input.GetKey(_xRightKeyCode))
         {
             _xMovement++;
         }
 
-        if (Input.GetKeyDown(_zFrontKeyCode))
-        {
-            _zMovement--;
-        }
-
-        if (Input.GetKeyDown(_zBackKeyCode))
+        if (Input.GetKey(_zFrontKeyCode))
         {
             _zMovement++;
         }
 
-        Vector3 playerMove = new Vector3(_xMovement, 0.0f, _zMovement);
-
-        transform.Translate(playerMove * _playerSpeed);
-    }
-
-    public void PlayerJump()
-    {
-        if (Input.GetKeyDown(_jumpKeyCode))
+        if (Input.GetKey(_zBackKeyCode))
         {
-            _player_RigidBody.AddForce(Vector3.up  * _playerForce);
+            _zMovement--;
         }
-    }
-
-    void Start()
-    {
-        _player_RigidBody = GetComponent<Rigidbody>(); // Get Player's RigidBody Component
-    }
-
-    void Update()
-    {
-        PlayerMove();
-        PlayerJump();
-    }
-}
-
-/*
-
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    // Variables for Player Right + Left Movement 
-        private float _xMovement;
-        private float _zMovement;
-
-    // Variables for Editable KeyCodes 
-    [Header("Left & Right Movement")]
-    [SerializeField] private List<KeyCode> _xKeyCodes;
-    [SerializeField] private List<KeyCode> _zKeyCodes;
-
-    [Header("Jump Keycode")]
-    [SerializeField] private KeyCode _jumpKeyCode = KeyCode.Space; 
-
-
-    // Player's speed + force
-    [Header("Speed")]
-    [SerializeField] private float _playerSpeed = 0.1f;
-
-    [Header("Speed")]
-    [SerializeField] private float _playerForce = 500f;
-
-    // Varibale to get Player's RigidBody Component
-        private Rigidbody _player_RigidBody;
-
-    public void PlayerMove()
-    {
-
-        // Receive position input from Player - is this correct ??? - Delete
-        float _xMovement = Input.GetAxis(_xKeyCodes); //Assigns ad key or left/right arrow key
-        float _zMovement = Input.GetAxis("Vertical"); //Assigns ws or up/down arrow key 
 
         Vector3 playerMove = new Vector3(_xMovement, 0.0f, _zMovement);
 
@@ -136,6 +77,3 @@ public class PlayerController : MonoBehaviour
         PlayerJump();
     }
 }
-
-
-*/
