e7184581f3474f886be35ca2a1a4226dfb3312a2
Gayatri-beep <104546498@student.swin.edu.au>
2026-04-03 20:17:36 +1100

    Added collision code

--- Phantom vs Opera/Assets/Scripts/AudienceSupport/AudienceSupport.cs
+++ Phantom vs Opera/Assets/Scripts/AudienceSupport/AudienceSupport.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class AudienceSupport : MonoBehaviour
 {
     // Private Varialbes 
-        private float _audienceSupport;
+        private float _audienceSupport = 10; //don't know if this should be the value - Delete
+    public void ManageAudienceSupport()
+    {
+        
+    }
 
     void Start() //Idk if i need this - Delete
     {
