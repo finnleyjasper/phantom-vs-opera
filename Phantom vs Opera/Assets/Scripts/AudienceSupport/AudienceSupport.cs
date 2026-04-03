37afe3fe6df5fe27f08b812ffe5bae226f532e63
Gayatri-beep <104546498@student.swin.edu.au>
2026-04-03 13:04:42 +1100

    Added AudienceSupport script
    Added AudienceSupport script

---
+++ Phantom vs Opera/Assets/Scripts/AudienceSupport/AudienceSupport.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudienceSupport : MonoBehaviour
+{
+    // Private Varialbes 
+        private float _audienceSupport;
+
+    void Start() //Idk if i need this - Delete
+    {
+        
+    }
+
+    void Update() //Idk if i need this - Delete
+    {
+        
+    }
+}
