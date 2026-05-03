0a2e60b5f53f37eb579131231fe2b54d11f812be
elsomoose <113397833+elsomoose@users.noreply.github.com>
2026-05-03 19:51:30 +1000

    added catie art

---
+++ Phantom vs Opera/Assets/Scripts/Animations/animationcontroller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class animationcontroller : MonoBehaviour
+{
+    public GameObject earth, room, moon;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
