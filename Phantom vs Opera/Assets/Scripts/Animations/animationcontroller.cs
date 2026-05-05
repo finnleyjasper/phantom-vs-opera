0809431c9c32b100aee5d6fd6adda55694b3a73d
elsomoose <113397833+elsomoose@users.noreply.github.com>
2026-05-05 12:57:18 +1000

    animation script and adjustments

--- Phantom vs Opera/Assets/Scripts/Animations/animationcontroller.cs
+++ Phantom vs Opera/Assets/Scripts/Animations/animationcontroller.cs
@@ -3,15 +3,34 @@ using UnityEngine;
 public class animationcontroller : MonoBehaviour
 {
     public GameObject earth, room, moon;
+    private Vector3 test;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            activateanimation(earth, "earth");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            activateanimation(room, "room");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            activateanimation(moon, "moon");
+        }
+
+    }
+
+    void activateanimation (GameObject obj, string animname)
+    {
+        obj.GetComponent<Animator>().Play(animname);
     }
 }
